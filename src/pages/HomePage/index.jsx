d4c9858212af1d90fe7c827936bbff90a23e61b9
Rayen Mabrouk <mabroukr911@gmail.com>
2023-11-06 12:58:23 +0100

    performance fix

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,6 +1,5 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy } from "react";
 import "react-toastify/dist/ReactToastify.css";
-import Loader from "../UI/Loader";
 const Main = lazy(() => import("./componants/main"));
 const Nav = lazy(() => import("./componants/nav"));
 
@@ -8,9 +7,7 @@ export default function HomePage() {
   return (
     <>
       <Nav />
-      <Suspense fallback={<Loader className="home_loader" />}>
-        <Main />
-      </Suspense>
+      <Main />
     </>
   );
 }
